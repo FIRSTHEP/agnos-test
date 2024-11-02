0e3765c2059b07a42b63bb1129ded5605a8e9ca1
Adithep Sudcharee <adithep.s@rznet.com>
2024-11-02 08:20:21 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ import PatientForm from '../components/PatientForm';
 import StaffView from '../components/StaffView';
 import Image from 'next/image';
 
-
 export default function Home() {
   const [loading, setLoading] = useState<boolean>(false);
   const [showForms, setShowForms] = useState<boolean>(false);
-
 
   const handleButtonClick = (url: string) => {
     setLoading(true);
@@ -28,12 +26,12 @@ export default function Home() {
       <div className="flex flex-col items-center flex-grow">
         <Image
           src="/agnos_logo.webp"
-          alt="Description of image"
+          alt="Agnos Logo"
           width={500}
           height={300}
-          priority={true} />
+          priority
+        />
         <h1 className="text-5xl font-bold text-center text-gray-800 mb-4">Agnos Home Work</h1>
-
         <p className="text-lg text-gray-700 text-center mb-6">Please choose a role:</p>
 
         <div className="flex justify-center items-start w-full max-w-4xl relative">
@@ -49,12 +47,14 @@ export default function Home() {
           <button
             onClick={() => handleButtonClick("/patient")}
             className="btn flex-1 bg-blue-500 hover:bg-blue-600 text-white font-semibold text-2xl py-4 rounded-lg shadow-lg transition duration-200 text-center"
+            aria-label="Open Patient View"
           >
             {loading ? 'Loading...' : 'Patient'}
           </button>
           <button
             onClick={() => handleButtonClick("/staff")}
             className="btn flex-1 bg-green-500 hover:bg-green-600 text-white font-semibold text-2xl py-4 rounded-lg shadow-lg transition duration-200 text-center"
+            aria-label="Open Staff View"
           >
             {loading ? 'Loading...' : 'Staff'}
           </button>
@@ -62,7 +62,7 @@ export default function Home() {
 
         {loading && (
           <div className="loader mb-6">
-            <div className="spinner-border animate-spin inline-block w-10 h-10 border-4 rounded-full border-t-transparent" role="status">
+            <div className="spinner-border animate-spin inline-block w-10 h-10 border-4 rounded-full border-t-transparent border-blue-500" role="status">
               <span className="sr-only">Loading...</span>
             </div>
           </div>
@@ -74,10 +74,12 @@ export default function Home() {
             checked={showForms}
             onChange={handleCheckboxChange}
             className="mr-2"
+            aria-label="Toggle Dev Mode"
           />
           Dev Mode
         </label>
       </div>
+
       <footer className="bg-[rgb(30,42,55)] border-t border-gray-300 shadow-lg text-center w-full mt-auto p-4">
         <p className="text-white font-semibold">Developed by ADITHEP SUDCHAREE</p>
         <p className="text-gray-300 text-sm">© {new Date().getFullYear()} Agnos Home Work. All rights reserved.</p>
